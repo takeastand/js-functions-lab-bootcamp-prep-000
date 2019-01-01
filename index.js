d7b50bec5cd7bb9ec085209b6860a80340b5a408
christopher.tero Tero <christopher.tero@gmail.com>
2019-01-01 04:56:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo() {
-  var name = "you";
+function happyHolidaysTo(name) {
   return 'Happy holidays, ${name}!'
 }
 
